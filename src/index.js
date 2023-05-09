2965fe6e643d7f83072996827839a311fa93f23d
GAGV03 <gusale03@gmail.com>
2023-05-08 19:47:44 -0600

    a

--- src/index.js
+++ src/index.js
@@ -5,16 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 var element = React.createElement('h1',{className:'greeting'},'Hello world!');
+var element2 = React.createElement('h2',{className:'greeting'},'Segundo intento');
 ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(element2, document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render( <React.StrictMode> <App /> </React.StrictMode>, document.getElementById('root'));
+
 reportWebVitals();
